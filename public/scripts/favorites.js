efb917ece5208d4f0421ffb53e9900af92295591
rbizimungu <143827106+rbizimungu@users.noreply.github.com>
2024-05-16 16:33:32 +0000

    Moved the animal page css to a new css file and updated the like button

--- public/scripts/favorites.js
+++ public/scripts/favorites.js
@@ -1,20 +1,9 @@
 function toggleFavorite(animalId) {
-    fetch("/api/favorites", {
+    return fetch("/api/favorites", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({ animalId: animalId })
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                console.log('Favorite updated successfully');
-            } else {
-                console.error('Error updating favorite:', data.message);
-            }
-        })
-        .catch(error => {
-            console.error('Error updating favorite:', error);
-        });
+    });
 }
